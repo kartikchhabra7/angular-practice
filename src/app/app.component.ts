e8072ba5f73831ef968c2dd69ed8085dc1c3bb51
kartik chhabra <chhabrakartik6@gmail.com>
2024-03-08 12:49:46 +0530

    🚚 refactor : refactored the code without changing the functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
-import { TodoWithTemplateReferenceComponent } from './todo-with-template-reference/todo-with-template-reference.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TodoWithTemplateReferenceComponent],
+  imports: [ RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { TodoWithTemplateReferenceComponent } from './todo-with-template-reference/todo-with-template-reference.component';
+import { TodoWithTwoWayDatabindingComponent } from './todo-with-two-way-databinding/todo-with-two-way-databinding.component';
+import { SearchFilterComponent } from './search-filter/search-filter.component';
+import { SearchFilterInApiComponent } from './search-filter-in-api/search-filter-in-api.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TodoWithTemplateReferenceComponent,
+  },
+  {
+    path: 'todo',
+    component: TodoWithTwoWayDatabindingComponent,
+  },
+  {
+    path: 'search-filter',
+    component: SearchFilterComponent,
+  },
+  {
+    path: 'search-filter-api',
+    component: SearchFilterInApiComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
